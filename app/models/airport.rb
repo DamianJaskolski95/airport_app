9ced510ee3f8cbe4289c79c48897958c26905c6b
DamianKleska95 <damian.kleska95@gmail.com>
2019-01-29 18:23:54 +0100

    Airport deleting fix

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,5 +1,7 @@
 class Airport < ApplicationRecord
-  has_many :flights, dependent: :destroy
+  #has_many :flights, dependent: :destroy
+  has_many :airport_sources, :class_name => 'Flight', :foreign_key => "airport_source_id", dependent: :destroy
+  has_many :airport_destins, :class_name => 'Flight', :foreign_key => "airport_dest_id", dependent: :destroy
 
   CITY_FORMAT = /\A([A-Z]{1}[a-z]+[\ ]{0,1}){0,5}\z/
   validates :city, presence: true, format: {with: CITY_FORMAT, message: "wrong format"}
